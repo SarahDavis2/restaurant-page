72639c1cb93a5ddfeeafb75646515ea2c3c90b34
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-10-10 13:56:25 -0500

    Fix: clean code

--- src/index.js
+++ src/index.js
@@ -7,11 +7,23 @@ import {Menu} from "./scripts/Menu.js";
 import "./styles/reset.css";
 import "./styles.css";
 
+class LoadPage {
+    constructor() {
+        this.board = document.querySelector('.board');
+        new HomePage;
+    }
+    clear() {
+        this.board.innerHTML = '';
+    }
+}
+new LoadPage();
+
 // for tests
 // new HomePage();
 // new About();
-new Menu();
-
+// new Menu();
 
 // TODO: ADD IMAGES TO MENU WITH WEBPACK SUPPORT
 // ALSO AM I SUPPOSE TO ADD STYLING THROUGH JAVASCRIPT AND NOT CSS?
+// MAY NEED TO EXTEND ENTIRE PAGE TO WINDOW HEIGHT
+// MAKE SURE PAGE VARIABLES AER PRIVATE?
