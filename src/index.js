245aa2264c6764493ba5712fd807bcc3f579c592
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-10-10 11:53:56 -0500

    Feat: Page class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 // scripts
-import { HomePage } from "./scripts/HomePage.js";
+import {Page} from "./scripts/HomePage.js";
+// import { HomePage } from "./scripts/HomePage.js";
 
 // styles
 import "./styles/reset.css";
 import "./styles.css";
 
-const home = new HomePage();
+const page = new Page();
+page.addTitle("TEST");
+page.addCard("YUH", "para");
+
+// const home = new HomePage();
--- src/scripts/HomePage.js
+++ src/scripts/HomePage.js
@@ -1,7 +1,44 @@
-class HomePage {
+class Page {
     constructor() {
-        console.log('hello');
+        this.board = document.querySelector('.board');
+    }
+    addTitle(inputTitle) {
+        const title = document.createElement('div');
+        const h1 = document.createElement('h1');
+
+        title.classList.add('card', 'title');
+        h1.textContent = inputTitle;
+
+        this.board.appendChild(title);
+        title.appendChild(h1);
+    }
+    addCard(inputHeader, inputBody) {
+        const card = document.createElement('div');
+        const header = document.createElement('h2');
+        const body = document.createElement('p');
+
+        card.classList.add('card');
+        header.textContent = inputHeader;
+        body.textContent = inputBody;
+
+        this.board.appendChild(card);
+        card.appendChild(header);
+        card.appendChild(body);
     }
 }
 
-export {HomePage};
+export {Page};
+
+// class HomePage {
+//     constructor() {
+//         console.log('hello');
+//         const div = document.createElement('div');
+//         div.textContent = "TEST";
+//         const board = document.querySelector('.board');
+//         board.appendChild(div);
+//     }
+// }
+
+// export {HomePage};
+
+// We creating stuff inside the board element
