1084f3abce07e391d375a165f66d62edaebdf1d0
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-10-10 12:34:27 -0500

    Feat: HomePage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 // scripts
-import {Page} from "./scripts/HomePage.js";
+import {HomePage} from "./scripts/HomePage.js";
 // import { HomePage } from "./scripts/HomePage.js";
 
 // styles
 import "./styles/reset.css";
 import "./styles.css";
 
-const page = new Page();
-page.addTitle("TEST");
-page.addCard("YUH", "para");
+// for tests
+new HomePage();
 
-// const home = new HomePage();
--- src/scripts/HomePage.js
+++ src/scripts/HomePage.js
@@ -27,18 +27,46 @@ class Page {
     }
 }
 
-export {Page};
 
-// class HomePage {
-//     constructor() {
-//         console.log('hello');
-//         const div = document.createElement('div');
-//         div.textContent = "TEST";
-//         const board = document.querySelector('.board');
-//         board.appendChild(div);
-//     }
-// }
+class HomePage extends Page {
+    constructor() {
+        super();
+        const HOURS = [
+            "Sunday: 11am-5pm", 
+            "Monday: 7am-3pm", 
+            "Tuesday: 7am-5pm", 
+            "Wednesday: 7am-5pm",
+            "Thursday: 7am-5pm",
+            "Friday: 7am-10pm",
+            "Saturday: 11am-10pm",
+        ];
 
-// export {HomePage};
+        this.addTitle("Slushy's Smoothie Bar");
+        this.addCard("Customer Review", '"This bar has the most delicious smoothies and best bartenders!"');
+        this.addHours("Hours", HOURS);
+        this.addCard("Location", "6769 Top Bars Street, Barstool, NY, 69420");
+    }   
+    addHours(inputHeader, list) {
+        const card = document.createElement('div');
+        const header = document.createElement('h2');
+        const ul = document.createElement('ul');
+
+        card.classList.add('card');
+        header.textContent = inputHeader;
+        ul.setAttribute('role', 'list');
+        list.forEach((item) => {
+            const addItem = document.createElement('li');
+            addItem.textContent = item;
+            ul.appendChild(addItem);
+        });
+
+        this.board.appendChild(card);
+        card.appendChild(header);
+        card.appendChild(ul);
+    }
+}
+
+export {HomePage};
+
 
 // We creating stuff inside the board element
