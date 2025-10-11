a3307a76d240e0cbd8ba54c18b6f34a61b54b156
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-10-11 02:36:28 -0500

    Feat: menu

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ This program is to demonstrate new methods of organized code through webpack, an
 ## Image Cited
  - Fresh Fruits with Yogurt in Clear Glass by [Nicola Barts](https://www.pexels.com/photo/fresh-fruits-with-yogurt-in-clear-glass-7937483/)
  - Avocado Toast Served on White Plate by [Julias Torten und Törtchen](https://www.pexels.com/photo/avocado-toast-served-on-white-plate-10464867/)
+ - Sandwich With Vegetables on White Ceramic Plate by [Jaymantri](https://www.pexels.com/photo/food-breakfast-fork-bagel-6492/)
+ - Person Putting an Ice cube with a Mint Leaf into a Green Juice by [Jacqueline Spotto](https://www.pexels.com/photo/person-putting-an-ice-cube-with-a-mint-leaf-into-a-green-juice-12018245/)
+ - Smoothie Decorated with Strawberry Slices and Flowers by [Antoni Shkraba Studio](https://www.pexels.com/photo/smoothie-decorated-with-strawberry-slices-and-flowers-4753640/)
 
 ## Live Demo
 [Live Demo Screenshot]()
--- src/scripts/Menu.js
+++ src/scripts/Menu.js
@@ -1,27 +1,37 @@
 import {Page} from "./Page.js";
+import berry from "../assets/pics/berry-smoothie.jpg";
+import green from "../assets/pics/green-smoothie.jpg";
+import toast from "../assets/pics/avocado-toast.jpg";
+import bagel from "../assets/pics/bagel.jpg";
 
 class Menu extends Page {
     constructor() {
         super();
     }
-    #addItem(inputName, inputDesc, inputPrice) {
+    #addItem(inputName, inputDesc, inputPrice, inputImg) {
         const card = this.addBlankCard(inputName);
+        const img = document.createElement('img');
         const desc = document.createElement('p');
         const price = document.createElement('p');
 
+        card.classList.add('item');
         price.classList.add('price');
+        img.src = inputImg;
         desc.textContent = inputDesc;
         price.textContent = inputPrice;
 
         card.appendChild(desc);
         card.appendChild(price);
+        card.appendChild(img);
     }
     render() {
         this.addTitle("Menu");
         this.addSubTitle("Drinks");
-        this.#addItem("Barry's Blastoff!", '"This mixed berry smoothie is ABSOLUTELY SCRUMPTIOUS!" -Barry', "$5");
+        this.#addItem("Barry's Blastoff!", '"This mixed berry smoothie is ABSOLUTELY SCRUMPTIOUS!" -Barry', "$7", berry);
+        this.#addItem("Greenie's Garden", 'This is the best grounded green leaf in town!"-Stone', "$6", green);
         this.addSubTitle("Food");
-        this.#addItem("Avocado Toast", `"OMG. IT'S SO Creamy! :)" -Bernie`, "$7")
+        this.#addItem("Avocado Toast", `"OMG. IT'S SO Creamy! :)" -Bernie`, "$7", toast);
+        this.#addItem("Bagel Byte", "This bagel is larger than a bit, because you're taking large bytes!", "$5", bagel);
     }
 }
 
